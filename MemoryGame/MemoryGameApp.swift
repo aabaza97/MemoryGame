196b5aa8524dd32406e27b73e90e3ccf09183d9c
Ahmed Abaza <aabaza32@hotmail.com>
2021-06-14 14:15:05 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Ahmed Abaza on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
